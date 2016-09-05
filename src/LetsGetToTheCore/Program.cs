c32499f5f5b6d295649f4ecbb4809c4ed71f7f46
Ricky Simmons <Ricky.E.Simmons@disney.com>
2016-09-04 23:25:34 -0400

    Add project files.

---
+++ src/LetsGetToTheCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsGetToTheCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!, I love you to the CORE.");
+        }
+    }
+}
